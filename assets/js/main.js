09234643240fc2f0b70d0982cdbea6d605fed5dc
Kraken <akzeebose@gmail.com>
2025-09-23 22:27:37 +0530

    set

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,273 +1,331 @@
-(function ($)
-  { "use strict"
-  
-/* 1. Proloder */
-    $(window).on('load', function () {
-      $('#preloader-active').delay(50).fadeOut('slow');
-      $('body').delay(50).css({
-        'overflow': 'visible'
-      });
-    });
+(function ($) {
+  "use strict";
 
-/* 2. sticky And Scroll UP */
-    $(window).on('scroll', function () {
-      var scroll = $(window).scrollTop();
-      if (scroll < 400) {
-        $(".header-sticky").removeClass("sticky-bar");
-        $('#back-top').fadeOut(500);
-      } else {
-        $(".header-sticky").addClass("sticky-bar");
-        $('#back-top').fadeIn(500);
-      }
+  /* 1. Proloder */
+  $(window).on("load", function () {
+    $("#preloader-active").delay(50).fadeOut("slow");
+    $("body").delay(50).css({
+      overflow: "visible",
     });
+  });
+
+  /* 2. sticky And Scroll UP */
+  $(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 400) {
+      $(".header-sticky").removeClass("sticky-bar");
+      $("#back-top").fadeOut(500);
+    } else {
+      $(".header-sticky").addClass("sticky-bar");
+      $("#back-top").fadeIn(500);
+    }
+  });
 
   // Scroll Up
-    $('#back-top a').on("click", function () {
-      $('body,html').animate({
-        scrollTop: 0
-      }, 10);
-      return false;
+  $("#back-top a").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      10
+    );
+    return false;
+  });
+
+  /* 3. slick \ */
+  // mobile_menu
+  var menu = $("ul#navigation");
+  if (menu.length) {
+    menu.slicknav({
+      prependTo: ".mobile_menu",
+      closedSymbol: "+",
+      openedSymbol: "-",
     });
-  
-
-/* 3. slick \ */
-// mobile_menu
-    var menu = $('ul#navigation');
-    if(menu.length){
-      menu.slicknav({
-        prependTo: ".mobile_menu",
-        closedSymbol: '+',
-        openedSymbol:'-'
-      });
-    };
-
-/* 4. MainSlider-1 */
-    // h1-hero-active
-    function mainSlider() {
-      var BasicSlider = $('.slider-active');
-      BasicSlider.on('init', function (e, slick) {
-        var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-        doAnimations($firstAnimatingElements);
-      });
-      BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-        var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-        doAnimations($animatingElements);
-      });
-      BasicSlider.slick({
-        autoplay: true,
-        autoplaySpeed: 5000,
-        dots: false,
-        fade: true,
-        arrows: false, 
-        prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-            }
-          },
-          {
-            breakpoint: 991,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows: false
-            }
-          },
-          {
-            breakpoint: 767,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows: false
-            }
-          }
-        ]
-      });
-
-      function doAnimations(elements) {
-        var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        elements.each(function () {
-          var $this = $(this);
-          var $animationDelay = $this.data('delay');
-          var $animationType = 'animated ' + $this.data('animation');
-          $this.css({
-            'animation-delay': $animationDelay,
-            '-webkit-animation-delay': $animationDelay
-          });
-          $this.addClass($animationType).one(animationEndEvents, function () {
-            $this.removeClass($animationType);
-          });
-        });
-      }
-    }
-    mainSlider();
-
-
-    function newSlider() {
-      $('.new-slider').slick({
-          autoplay: true,
-          autoplaySpeed: 4000,
-          dots: false,
-          arrows: false,
-          infinite: true,
-          speed: 500,
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-          nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-          responsive: [
-              {
-                  breakpoint: 1024,
-                  settings: {
-                      slidesToShow: 2,
-                      slidesToScroll: 1
-                  }
-              },
-              {
-                  breakpoint: 768,
-                  settings: {
-                      slidesToShow: 1,
-                      slidesToScroll: 1,
-                      arrows: false
-                  }
-              }
-          ]
-      });
   }
-      newSlider();
-  
-/* 5. Testimonial Active*/
 
-/* 4. Testimonial Active*/
-    var testimonial = $('.h1-testimonial-active');
-    if(testimonial.length){
-    testimonial.slick({
-        dots: false,
-        infinite: true,
-        speed: 1000,
-        autoplay:false,
-        loop:true,
-        arrows: false,
-        prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: false,
-              arrow:false
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows:false
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows:false,
-            }
-          }
-        ]
-      });
-    }
-
-
-
-/* 6. Nice Selectorp  */
-  var nice_Select = $('select');
-    if(nice_Select.length){
-      nice_Select.niceSelect();
-    }
-
-/* 7. data-background */
-    $("[data-background]").each(function () {
-      $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
-      });
-
-
-/* 10. WOW active */
-    new WOW().init();
-
-// 11. ---- Mailchimp js --------//  
-    function mailChimp() {
-      $('#mc_embed_signup').find('form').ajaxChimp();
-    }
-    mailChimp();
-
-
-// 12 Pop Up Img
-    var popUp = $('.single_gallery_part, .img-pop-up');
-      if(popUp.length){
-        popUp.magnificPopup({
-          type: 'image',
-          gallery:{
-            enabled:true
-          }
-        });
-      }
-// 12 Pop Up Video
-    var popUp = $('.popup-video');
-    if(popUp.length){
-      popUp.magnificPopup({
-        type: 'iframe'
-      });
-    }
-
-/* 13. counterUp*/
-    $('.counter').counterUp({
-      delay: 10,
-      time: 3000
+  /* 4. MainSlider-1 */
+  // h1-hero-active
+  function mainSlider() {
+    var BasicSlider = $(".slider-active");
+    BasicSlider.on("init", function (e, slick) {
+      var $firstAnimatingElements = $(".single-slider:first-child").find(
+        "[data-animation]"
+      );
+      doAnimations($firstAnimatingElements);
     });
-    /*------------------
+    BasicSlider.on(
+      "beforeChange",
+      function (e, slick, currentSlide, nextSlide) {
+        var $animatingElements = $(
+          '.single-slider[data-slick-index="' + nextSlide + '"]'
+        ).find("[data-animation]");
+        doAnimations($animatingElements);
+      }
+    );
+    BasicSlider.slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      dots: false,
+      fade: true,
+      arrows: false,
+      prevArrow:
+        '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+          },
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          },
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          },
+        },
+      ],
+    });
+
+    function doAnimations(elements) {
+      var animationEndEvents =
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+      elements.each(function () {
+        var $this = $(this);
+        var $animationDelay = $this.data("delay");
+        var $animationType = "animated " + $this.data("animation");
+        $this.css({
+          "animation-delay": $animationDelay,
+          "-webkit-animation-delay": $animationDelay,
+        });
+        $this.addClass($animationType).one(animationEndEvents, function () {
+          $this.removeClass($animationType);
+        });
+      });
+    }
+  }
+  mainSlider();
+
+  function newSlider() {
+    $(".new-slider").slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      dots: false,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow:
+        '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          },
+        },
+      ],
+    });
+  }
+  newSlider();
+
+  /* 5. Testimonial Active*/
+
+  /* 4. Testimonial Active*/
+  var testimonial = $(".h1-testimonial-active");
+  if (testimonial.length) {
+    testimonial.slick({
+      dots: false,
+      infinite: true,
+      speed: 1000,
+      autoplay: false,
+      loop: true,
+      arrows: false,
+      prevArrow:
+        '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+      nextArrow:
+        '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+            arrow: false,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          },
+        },
+      ],
+    });
+  }
+
+  /* 6. Nice Selectorp  */
+  var nice_Select = $("select");
+  if (nice_Select.length) {
+    nice_Select.niceSelect();
+  }
+
+  /* 7. data-background */
+  $("[data-background]").each(function () {
+    $(this).css(
+      "background-image",
+      "url(" + $(this).attr("data-background") + ")"
+    );
+  });
+
+  /* 10. WOW active */
+  new WOW().init();
+
+  // 11. ---- Mailchimp js --------//
+  function mailChimp() {}
+  mailChimp();
+
+  // 12 Pop Up Img
+  var popUp = $(".single_gallery_part, .img-pop-up");
+  if (popUp.length) {
+    popUp.magnificPopup({
+      type: "image",
+      gallery: {
+        enabled: true,
+      },
+    });
+  }
+  // 12 Pop Up Video
+  var popUp = $(".popup-video");
+  if (popUp.length) {
+    popUp.magnificPopup({
+      type: "iframe",
+    });
+  }
+
+  /* 13. counterUp*/
+  $(".counter").counterUp({
+    delay: 10,
+    time: 3000,
+  });
+  /*------------------
         CountDown
     --------------------*/
-    // For demo preview
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-    if(mm == 12) {
-        mm = '01';
-        yyyy = yyyy + 1;
-    } else {
-        mm = parseInt(mm) + 1;
-        mm = String(mm).padStart(2, '0');
-    }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
-    // For demo preview end
-    
+  // For demo preview
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
+  if (mm == 12) {
+    mm = "01";
+    yyyy = yyyy + 1;
+  } else {
+    mm = parseInt(mm) + 1;
+    mm = String(mm).padStart(2, "0");
+  }
+  var timerdate = mm + "/" + dd + "/" + yyyy;
+  // For demo preview end
 
-    // Use this for real timer date
-    /*  var timerdate = "2020/01/01"; */
+  // Use this for real timer date
+  /*  var timerdate = "2020/01/01"; */
 
-	$("#countdown").countdown(timerdate, function(event) {
-        $(this).html(event.strftime("<div class='cd-item'><span>%D</span><p>Days</p> </div>" + "<div class='cd-item'><span>%H</span><p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span><p>Min</p> </div>" + "<div class='cd-item'><span>%S</span><p>Sec</p> </div>"));
-    });
+  $("#countdown").countdown(timerdate, function (event) {
+    $(this).html(
+      event.strftime(
+        "<div class='cd-item'><span>%D</span><p>Days</p> </div>" +
+          "<div class='cd-item'><span>%H</span><p>Hrs</p> </div>" +
+          "<div class='cd-item'><span>%M</span><p>Min</p> </div>" +
+          "<div class='cd-item'><span>%S</span><p>Sec</p> </div>"
+      )
+    );
+  });
 
+  /* 14. Datepicker */
+  $("#datepicker1").datepicker();
 
+  // 15. Time Picker
+  $("#timepicker").timepicker();
 
-    
-/* 14. Datepicker */
-  $('#datepicker1').datepicker();
+  var contactForm = function () {
+    document
+      .getElementById("contact-form")
+      .addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent default form submission
 
-// 15. Time Picker
-  $('#timepicker').timepicker();
+        // Get form values
+        let name = document.getElementById("c-name").value;
+        let phone = document.getElementById("c-phone").value;
+        let message = document.getElementById("c-message").value;
 
+        // Validate inputs
+        if (!name || !phone || !message) {
+          document.getElementById("form-message-warning").innerText =
+            "All fields are required.";
+          document.getElementById("form-message-warning").style.display =
+            "block";
+          return;
+        }
 
+        // Send email using EmailJS
+        emailjs
+          .send("mohans", "mohansT", {
+            name: name,
+            phone: phone,
+            message: message,
+          })
+          .then(
+            function (response) {
+              console.log("SUCCESS!", response.status, response.text);
+              alert("Email sent successfully!");
+            },
+            function (error) {
+              console.error("Failed to send email:", error);
+              alert("Failed to send email.");
+            }
+          );
+
+        // Reset form fields after submission
+        document.getElementById("contact-form").reset();
+      });
+  };
+  contactForm();
 })(jQuery);
